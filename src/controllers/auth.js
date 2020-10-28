1bc392d41e509dd9c9529435c2a58aa43dca82ce
Miguel <gimenezgimenezmiguel@gmail.com>
2020-10-28 15:36:09 +0100

    fix auth controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,19 +1,14 @@
 // const apiService = require("../services/api")
 const axios = require('axios')
-
+const jwtDecode = require('jwt-decode');
 const { API_URL } = require('../config')
 
 const login = async (username, password) => {
   const url = `${API_URL}/api/login`
-  
-
-  
-  // "client_id": "axa",
-  // "client_secret": "s3cr3t"
-
-  const token = await axios.post(url, { username, password })
-
-  return token
+  const response = await axios.post(url, { client_id: username, client_secret: password })
+  const { token, type } = response.data
+  const { iat, exp } = jwtDecode(response.data.token);
+  return { token, type, expires_in: exp - iat }
 }
 
 module.exports = {
