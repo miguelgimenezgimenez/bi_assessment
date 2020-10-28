02a2c6b30edec6cf070966772a42fd4c08f4d240
Miguel <gimenezgimenezmiguel@gmail.com>
2020-10-28 18:18:23 +0100

    create api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,44 @@
-const clientId = process.env.CLIENT_ID
-const clientSecret = process.env.CLIENT_SECRET
-const apiUrl = process.env.API_URL
+const fetch = require('node-fetch');
+const redisClient = require('../redis-client');
+const { API_URL } = require('../config');
 
-class ApiService {
- 
-  login() {
+const apiService = {
+  get: async (endpoint, token) => {
+    const url = `${API_URL}/${endpoint}`
     
+    const cacheKey = endpoint
+    let storedData = await redisClient.getAsync(cacheKey)
+    if (storedData) {
+      storedData = JSON.parse(storedData)
+    }
+
+    let headers = {};
+    
+    if (token) {
+      headers.Authorization = 'Bearer ' + token
+    }
+
+    if (storedData && storedData.etag) {
+      headers['If-None-Match'] = storedData.etag;
+    }
+
+    const response = await fetch(url, {
+      headers
+    })
+
+    if (response.status === 304) {
+      console.info('from cache');
+      return storedData.data;
+    }
+    const data = await response.json()
+    const etag = response.headers.get('Etag')
+    if (etag) {
+      redisClient.setAsync(endpoint, JSON.stringify({ data, etag }));
+    }
+    return data;
   }
+
 }
 
 
-module.exports = new ApiService(clientId, clientSecret)
+module.exports = apiService
