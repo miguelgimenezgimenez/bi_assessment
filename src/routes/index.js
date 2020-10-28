170092e3cf9f2bddde2423e7cdf88ff129a4bcf2
Miguel <gimenezgimenezmiguel@gmail.com>
2020-10-28 16:49:41 +0100

    add clients route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/login', require('./auth'))
+router.use('/clients', require('./clients'))
 
 module.exports = router
  
