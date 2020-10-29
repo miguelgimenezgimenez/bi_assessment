f29dd86fdc3e56d60e1732b0407dfbdb44177bc0
Miguel <gimenezgimenezmiguel@gmail.com>
2020-10-29 10:47:44 +0100

    refactor error middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,13 +2,12 @@ const logger = require('../utils/logger')
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (error, req, res, next) => {
-  let status = error || 500
+  let status = error.statusCode || 500
   let message = error.message
-  let code = 0
-  if (error.isAxiosError) {
-    status = error.response.data.statusCode
-    message = error.response.data.message
+  const output = { message }
+  if (typeof error.code !== 'undefined') {
+    output.code = error.code
   }
-  logger.log(message)
-  res.status(status).send({ message, code })
+  logger.log(`ERROR: ${message}`)
+  res.status(status).send(output)
 }
