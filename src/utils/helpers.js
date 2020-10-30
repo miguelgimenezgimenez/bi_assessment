ed81b6c6d09ab7aec724dd3538a048e0e45b0384
Miguel <gimenezgimenezmiguel@gmail.com>
2020-10-30 13:07:12 +0100

    add helper functions

---
+++ src/utils/helpers.js
@@ -0,0 +1,41 @@
+
+function sortBy(array, key) {
+  return [...array].sort((a, b) => {
+    if (a[key] > b[key]) {
+      return 1
+    }
+    if (a[key] < b[key]) {
+      return -1
+    }
+    return 0
+  })
+}
+
+function mergeClientPolicies(clients, policies) {
+  const sortedClients = sortBy(clients, 'id')
+  const sortedPolicies = sortBy(policies, 'clientId')
+
+  let clientIndex = 0
+  let policiesIndex = 0
+  const combinedData = []
+  while (clientIndex < sortedClients.length && policiesIndex < sortedPolicies.length) {
+    let client = { ...sortedClients[clientIndex], policies: [] }
+    while (sortedClients[clientIndex].id !== sortedPolicies[policiesIndex].clientId) {
+      combinedData.push(client)
+      clientIndex++
+    }
+    client = { ...sortedClients[clientIndex], policies: [] }
+    while (policiesIndex < sortedPolicies.length && client.id === sortedPolicies[policiesIndex].clientId) {
+      const { id, amountInsured, inceptionDate } = sortedPolicies[policiesIndex]
+      const policy = { id, amountInsured, inceptionDate }
+      client.policies.push(policy)
+      policiesIndex++
+    }
+    combinedData.push(client)
+    clientIndex++
+  }
+  return combinedData
+}
+
+
+module.exports = { mergeClientPolicies, sortBy }
