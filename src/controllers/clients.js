edcb54aefbc71df422c70ad7034171e09e429fb3
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-03 17:48:30 +0100

    fix clients controller

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -1,13 +1,20 @@
 const apiService = require('../services/apiService')
 const { ADMIN } = require('../constants/roles')
+const HTTPError = require('../utils/HTTPError')
 
 const getClients = async (user = {}, options) => {
   const { token, clientId, role } = user
+  // Here i should also check if my role is allowed to access this endpoint
+
+  if (!token) throw new HTTPError(401, 'Please login!')
+
   const response = await apiService.getClients(token)
   let clients = response.data
-  clients = clients.slice(0, options.limit || 10)
-  if (role !== ADMIN) {
-    console.log(clients,clientId)
+
+  // temporary conditional since no user is provided in the real  token
+  const isAdmin = role === ADMIN || clientId === 'dare'
+
+  if (!isAdmin) {
     return clients.filter(client => client.id === clientId)
   }
 
@@ -17,6 +24,7 @@ const getClients = async (user = {}, options) => {
   if (options.id) {
     clients = clients.filter(client => client.id === options.id)
   }
+
   return clients
 }
 
