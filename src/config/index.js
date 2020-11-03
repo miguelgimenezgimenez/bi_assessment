f10ca362504d9508b9efaca0382fb152534105c6
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-03 13:14:29 +0100

    fix config

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 
 const API_URL = process.env.API_URL
+const PORT = process.env.PORT
+const COOKIE_SECRET = process.env.COOKIE_SECRET
+
 
 module.exports = {
-  API_URL
+  API_URL,
+  PORT,
+  COOKIE_SECRET
 }
