98ef618f14e95c9c00e3a92546404241e5e331d3
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-03 17:49:13 +0100

    feat policies controller functionality

--- src/controllers/policies.js
+++ src/controllers/policies.js
@@ -1,10 +1,24 @@
 const { POLICIES } = require('../constants/endpoints')
-const apiService = require('../services/api')
+const apiService = require('../services/apiService')
+const { ADMIN } = require('../constants/roles')
 
-const getPolicies = async (token, options) => {
-  const endpoint = POLICIES
-  const response = await apiService.get(token)
+const getPolicies = async (user, options) => {
+  const { token, clientId, role } = user
 
+  const response = await apiService.get(POLICIES, token)
+  let policies = response.data
+  // temporary conditional since no user is provided in the real  token
+  const isAdmin = role === ADMIN || clientId === 'dare'
+
+  if (!isAdmin) {
+    return policies.filter(policy => policy.clientId === clientId)
+  }
+
+  if (options.id) {
+    policies = policies.filter((policy) => policy.clientId === options.id)
+  }
+
+  return policies
 }
 
 module.exports = {
