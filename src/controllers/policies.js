4ea16be1d78406b1d56f5c454caae6186988e6ac
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-03 18:26:32 +0100

    serialize policies correctly

--- src/controllers/policies.js
+++ src/controllers/policies.js
@@ -1,9 +1,11 @@
 const { POLICIES } = require('../constants/endpoints')
 const apiService = require('../services/apiService')
 const { ADMIN } = require('../constants/roles')
+const HTTPError = require('../utils/HTTPError')
 
-const getPolicies = async (user, options) => {
+const getPolicies = async (user = {}, options) => {
   const { token, clientId, role } = user
+  if (!token) throw new HTTPError(401, 'Please login!')
 
   const response = await apiService.get(POLICIES, token)
   let policies = response.data
@@ -18,7 +20,19 @@ const getPolicies = async (user, options) => {
     policies = policies.filter((policy) => policy.clientId === options.id)
   }
 
-  return policies
+  return policies.reduce((acc, next) => {
+    const shouldAddNext = !options.id || options.id === next.clientId
+    if (shouldAddNext) {
+      acc.push({
+        'id': next.id,
+        'amountInsured': next.amountInsured,
+        'email': next.email,
+        'inceptionDate': next.inceptionDate,
+        'installmentPayment': next.installmentPayment,
+      })
+      return acc
+    }
+  }, [])
 }
 
 module.exports = {
