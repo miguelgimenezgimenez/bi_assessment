bcac9fd29ccfb8b25b990acb7ab2f8e4bcfbcddc
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-04 11:35:38 +0100

    feat get policy client

--- src/controllers/policies.js
+++ src/controllers/policies.js
@@ -1,4 +1,4 @@
-const { POLICIES } = require('../constants/endpoints')
+const { CLIENTS, POLICIES } = require('../constants/endpoints')
 const apiService = require('../services/apiService')
 const { ADMIN, USER } = require('../constants/roles')
 const HTTPError = require('../utils/HTTPError')
@@ -7,12 +7,11 @@ const { paginate } = require('../utils/helpers')
 
 const getPolicies = async (user = {}, options) => {
   const { token, clientId, role } = user
-  // Here i should also check if my role is allowed to access this endpoint
 
   if (!token) throw new HTTPError(401, 'Please login!')
   // temporary client dare conditional since no user is provided in the real token
   const isAdmin = role === ADMIN || clientId === 'dare'
-  // if user is not admin and role doesnt have permissions or user is trying to access other users policies forbid
+  // if user is not admin and role doesnt have permissions or user is trying to access other users policies then forbid
   if (!isAdmin && (role !== USER || options.id && options.id !== clientId)) {
     throw new HTTPError(403, 'Role Doesn\'t have permissions to access this endpoint')
   }
@@ -34,6 +33,30 @@ const getPolicies = async (user = {}, options) => {
   return paginate(policies, options.page, options.limit)
 }
 
+const getPolicyClient = async (user = {}, options) => {
+  const { token, clientId, role } = user
+
+  if (!token) throw new HTTPError(401, 'Please login!')
+  // temporary client dare conditional since no user is provided in the real token
+  const isAdmin = role === ADMIN || clientId === 'dare'
+  // if user is not admin and role doesnt have permissions or user is trying to access other users policies forbid
+  if (!isAdmin && role !== USER) {
+    throw new HTTPError(403, 'Role Doesn\'t have permissions to access this endpoint')
+  }
+  const policies = await apiService.get(POLICIES, token)
+  const policy = policies.data.find(policy => policy.id === options.id)
+  if (!policy) throw new HTTPError(404, 'Policy not found')
+
+  const clients = await apiService.get(CLIENTS, token)
+
+  const client = clients.data.find(client => client.id === policy.clientId)
+
+  if (!client) throw new HTTPError(404, 'Client not found')
+
+  return client
+}
+
 module.exports = {
-  getPolicies
+  getPolicies,
+  getPolicyClient
 }
--- src/routes/policies.js
+++ src/routes/policies.js
@@ -4,10 +4,17 @@ const policiesController = require('../controllers/policies.js')
 
 
 // GET POLICIES //
-router.get('/:id?', asyncWrapper(async (req, res) => {
+router.get('/', asyncWrapper(async (req, res) => {
   const { query, session, params } = req
   const options = { ...query, ...params }
   const response = await policiesController.getPolicies(session.user, options)
+  res.json(response)
+}))
+// GET POLICY's Client//
+router.get('/:id', asyncWrapper(async (req, res) => {
+  const { query, session, params } = req
+  const options = { ...query, ...params }
+  const response = await policiesController.getPolicyClient(session.user, options)
   res.json(response)
 }))
 module.exports = router
