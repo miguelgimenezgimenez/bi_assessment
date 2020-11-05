1fc82c3798143c5359c2323c568f9641aba86e5f
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-05 08:43:24 +0100

    fix redis url

--- src/config/index.js
+++ src/config/index.js
@@ -4,11 +4,14 @@ const API_URL = process.env.API_URL
 const PORT = process.env.PORT
 const COOKIE_SECRET = process.env.COOKIE_SECRET
 const WHITE_LIST = process.env.WHITE_LIST
+const REDIS_URL = process.env.REDIS_URL
+
 
 
 module.exports = {
   API_URL,
   PORT,
   COOKIE_SECRET,
-  WHITE_LIST
+  WHITE_LIST,
+  REDIS_URL
 }
--- src/redisClient.js
+++ src/redisClient.js
@@ -1,6 +1,7 @@
 const redis = require('redis')
 const { promisify } = require('util')
-const client = redis.createClient(process.env.REDIS_URL)
+const { REDIS_URL } = require('./config')
+const client = redis.createClient(REDIS_URL)
 
 module.exports = {
   ...client,
