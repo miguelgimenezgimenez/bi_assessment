b664178cdc37423875a31507762242088107c763
Miguel <gimenezgimenezmiguel@gmail.com>
2020-11-06 07:04:45 +0100

    remove useless code

--- src/app.js
+++ src/app.js
@@ -19,10 +19,6 @@ const sess = {
   resave: true
 }
 
-if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
-  sess.cookie.secure = true // serve secure cookies
-}
 
 const corsOptions = {
   origin: WHITE_LIST,
